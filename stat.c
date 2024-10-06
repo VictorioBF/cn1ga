9a7436b1fe35914f16047e4be2c496db2df24399
VictorioBF <victoriofaraco@gmail.com>
2024-10-06 20:23:20 -0300

    changes to add ip function

--- stat.c
+++ stat.c
@@ -12,6 +12,7 @@
 
 #define ECHOMAX 255
 #define NUM_IPS 3 // Mínimo para o trabalho
+#define IP_SIZE 16
 
 // typedef int bool;
 // #define true 1
@@ -21,25 +22,17 @@
 // versão 0.15
 
 int interval = 1;
-int segue_o_baile = 1;
-char *ipAddresses[NUM_IPS] = {NULL};
+int stop = 0;
+char ipAddresses[NUM_IPS][IP_SIZE];
+int current_ip_index = 0;
 
 // Controle do array de IPs
 // Insere um novo IP no array
-void add_ip_address(char new_ip_address) {
-    int nextPosition = -1;
-
-    // Busca a próxima posição vazia
-    for (size_t i = 0; i < sizeof(ipAddresses) / sizeof(int); i++) {
-        if (ipAddresses[i] == NULL) {
-            nextPosition = i;
-            break;
-        }
-    }
-
-    // Checa para ver se é possível adicionar
-    if (nextPosition != -1) { // -1 quando não é possível (array já está preenchido)
-        *ipAddresses[nextPosition] = new_ip_address;
+void add_ip_address(const char *new_ip_address) {
+    if (current_ip_index < NUM_IPS) {
+        strncpy(ipAddresses[current_ip_index], new_ip_address, IP_SIZE - 1);
+        ipAddresses[current_ip_index][IP_SIZE - 1] = '\0'; // Garante que termina com o caractere nulo
+        current_ip_index++;
     }
 }
 
@@ -66,7 +59,7 @@ char *execute_command(const char *command) {
         strcpy(result, buffer);
         size = len + 1;
     }
-    printf("Comando executado, resultado: \n%s", result);
+    // printf("Comando executado, resultado: \n%s", result);
 
     pclose(pipe);
     return result;
@@ -83,10 +76,10 @@ void *sender_thread(void *arg) {
     char path[1035];
 
     // Criando Socket
-    printf("Criando socket para o envio...\n");
-    if ((sock = socket(AF_INET,SOCK_DGRAM,0)) < 0)
-        printf("Socket Falhou!!!\n");
-    printf("Socket deu boa!!!\n");
+    // printf("Criando socket para o envio...\n");
+    if ((sock = socket(AF_INET,SOCK_DGRAM,0)) < 0) return (void *)1;
+        // printf("Socket Falhou!!!\n");
+    // printf("Socket deu boa!!!\n");
 
     do {
         // Todo: Fazer uma ou algums funções para executar os comandos e montar a string
@@ -96,19 +89,20 @@ void *sender_thread(void *arg) {
         - definir pacotes
         */
         // Lendo informações para serem enviadas
-        printf("Thread de envio tentantdo executar comando...\n");
+        // printf("Thread de envio tentantdo executar comando...\n");
         strcpy(path, execute_command("vmstat"));
 
-        printf("Resultado do comando %s\n", path);
+        // printf("Resultado do comando %s\n", path);
         // Envio para os múltiplos endereços
-        for (size_t i = 0; i < sizeof(ipAddresses) / sizeof(int); i++) {
+        for (size_t i = 0; i < current_ip_index; i++) {
+            const char* ipAddress = ipAddresses[i];
             destiny.sin_family = AF_INET;
-            destiny.sin_addr.s_addr = inet_addr("127.0.0.1"); // Endereço
+            destiny.sin_addr.s_addr = inet_addr(ipAddress); // Endereço
             destiny.sin_port = htons(6000); // Porta de destino
             sendto(sock, path, ECHOMAX, 0, (struct sockaddr *)&destiny, sizeof(destiny)); // Envio das informações
         }
         sleep(interval);
-    } while(segue_o_baile = 1);
+    } while(stop = 0);
     close(sock);
 
     return (void *)0; // É necessário cast para evitar warning
@@ -117,9 +111,8 @@ void *sender_thread(void *arg) {
 // Função que rebe as informações
 void *receiver_thread(void *arg) {
     int sock;
-    /* Estrutura: familia + endereco IP + porta */
+    // Estrutura: familia + endereco IP + porta
     struct sockaddr_in me, from;
-    // socklen_t for MAC
     int adl = sizeof(me);
     char linha[ECHOMAX];
 
@@ -129,21 +122,23 @@ void *receiver_thread(void *arg) {
     else  
         printf("Servidor esperando mensagens...\n");
 
-    // Construcao da estrutura do endereco local, Preenchendo a estrutura socket me (familia, IP, porta)
+    // Construcao da estrutura do endereco local, preenchendo a estrutura socket me (familia, IP, porta)
     me.sin_family = AF_INET;
-    me.sin_addr.s_addr=htonl(INADDR_ANY); /* endereco IP local */
-    me.sin_port = htons(6000); /* porta local  */
+    me.sin_addr.s_addr=htonl(INADDR_ANY); // Endereco IP local
+    me.sin_port = htons(6000); // Porta local
 
     // Bind para o endereco local parametros(descritor socket, estrutura do endereco local, comprimento do endereco)
     if(-1 != bind(sock, (struct sockaddr *)&me, sizeof(me)))
-    do  {
-        /* Recebe mensagem do endereco remoto
-        parametros(descritor socket, dados, tamanho dos dados, flag, estrutura do socket remoto, tamanho da estrutura) */
-        recvfrom(sock, linha, ECHOMAX, 0, (struct sockaddr *)&from, &adl);
-        printf("recebido: %s\n", linha);
-    }
-    while(segue_o_baile = 1);
+        do  {
+            /* Recebe mensagem do endereco remoto
+            parametros(descritor socket, dados, tamanho dos dados, flag, estrutura do socket remoto, tamanho da estrutura) */
+            recvfrom(sock, linha, ECHOMAX, 0, (struct sockaddr *)&from, &adl);
+            printf("%s", linha);
+        }
+        while(stop = 0);
     else puts("Porta ocupada");
+
+    // Fecha o socket
     close(sock);
     return 0;
 }
@@ -153,7 +148,7 @@ int main() {
     int thread_args[2];
     int i;
 
-    interval = 2;
+    add_ip_address("127.0.0.1");
 
     i = 0;
     thread_args[i] = i;
