0fee6a315faf03be374144d1b9c4e59e6412d1c3
VictorioBF <victoriofaraco@gmail.com>
2024-10-07 19:18:38 -0300

    stable version 0.30

--- stat.c
+++ stat.c
@@ -15,12 +15,7 @@
 #define IP_SIZE 16
 #define APP_PORT 6001
 
-// typedef int bool;
-// #define true 1
-// #define false 0
-// bool error;
-
-// versão 0.21
+// versão 0.30
 
 int interval = 1;
 int stop = 0;
@@ -39,6 +34,8 @@ void add_ip_address(const char *new_ip_address) {
         strncpy(ipAddresses[current_ip_index], new_ip_address, IP_SIZE - 1);
         ipAddresses[current_ip_index][IP_SIZE - 1] = '\0'; // Garante que termina com o caractere nulo
         current_ip_index++;
+    } else {
+        printf("Limite de IPs alcançado. Não é possível adicionar mais.\n");
     }
 }
 
@@ -64,14 +61,15 @@ void get_local_ip(char *ip_buffer, size_t buffer_size) {
 
     // Converte para string
     struct in_addr *addr = (struct in_addr *)host_entry->h_addr_list[0];
-    strncpy(ip_buffer, inet_ntoa(*addr), buffer_size);
+    strncpy(ip_buffer, inet_ntoa(*addr), buffer_size - 1);
+    ip_buffer[buffer_size - 1] = '\0'; // Garante a terminação nula
 }
 
-// Recebe um comando e retorna a última linha dele
+// Recebe um comando e retorna o resultado completo
 char *execute_command(const char *command) {
     char buffer[128];
     char *result = NULL;
-    size_t size = 1;
+    size_t size = 0;
     FILE *pipe = popen(command, "r");
 
     if (!pipe) {
@@ -80,35 +78,60 @@ char *execute_command(const char *command) {
 
     while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
         size_t len = strlen(buffer);
-        char *new_result = realloc(result, size + len);
+        char *new_result = realloc(result, size + len + 1); // +1 para o terminador nulo
         if (new_result == NULL) {
             free(result);
             pclose(pipe);
             return NULL;
         }
         result = new_result;
-        strcpy(result, buffer);
-        size = len + 1;
+        strcpy(result + size, buffer);
+        size += len;
     }
-    // printf("Comando executado, resultado: \n%s", result);
 
     pclose(pipe);
     return result;
 }
 
+// Função para alterar o intervalo de envio
+void alter_interval() {
+    char input_buffer[100];
+    int new_interval;
+
+    printf("Digite o novo intervalo de envio (em segundos): ");
+
+    if (fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
+        // Remove o caractere de nova linha
+        input_buffer[strcspn(input_buffer, "\n")] = '\0';
+
+        // Tenta converter a entrada para um inteiro
+        new_interval = atoi(input_buffer);
+
+        if (new_interval > 0) {
+            pthread_mutex_lock(&input_lock);
+            interval = new_interval;
+            pthread_mutex_unlock(&input_lock);
+            printf("Intervalo atualizado para %d segundos.\n", interval);
+        } else {
+            printf("Entrada inválida! O intervalo deve ser um número inteiro positivo.\n");
+        }
+    } else {
+        printf("Erro ao ler a entrada.\n");
+    }
+}
+
 // Funções para as threads de envio e recebimento de pacotes
 // Função de envio de informações
 void *data_sender_thread(void *arg) {
-    int sock,i;
+    int sock, i;
     struct sockaddr_in destiny;
-    char linha[ECHOMAX+1];
+    char linha[ECHOMAX + 1];
     char servIP[ECHOMAX];
     FILE *fp;
     char path[1035];
 
     // Criando Socket
-    // printf("Criando socket para o envio...\n");
-    if ((sock = socket(AF_INET,SOCK_DGRAM,0)) < 0) {
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         printf("Socket Falhou!!!\n");
         return (void *)1;
     }
@@ -119,78 +142,84 @@ void *data_sender_thread(void *arg) {
         pthread_mutex_lock(&input_lock);
         while (pause_output) {
             pthread_cond_wait(&input_cond, &input_lock);
-            // printf("envio bloqueado");
         }
         pthread_mutex_unlock(&input_lock);
-        // Todo: Definir comandos (usei o vmstat como teste, faz parte das ferramentas do eBPF) e montar strings para mostrar em tela.
 
         // Execução dos comandos com dados
-        // printf("Thread de envio tentantdo executar comando...\n");
-        strcpy(path, execute_command("vmstat"));
+        char *command_result = execute_command("vmstat");
+        if (command_result != NULL) {
+            strncpy(path, command_result, sizeof(path) - 1);
+            path[sizeof(path) - 1] = '\0'; // Garante a terminação nula
+            free(command_result);
+        } else {
+            fprintf(stderr, "Erro ao executar o comando.\n");
+            path[0] = '\0'; // Ou alguma string padrão
+        }
 
-        // printf("Resultado do comando %s\n", path);
         // Envio para os múltiplos endereços
         for (size_t i = 0; i < current_ip_index; i++) {
-            const char* ipAddress = ipAddresses[i];
+            const char *ipAddress = ipAddresses[i];
             destiny.sin_family = AF_INET;
             destiny.sin_addr.s_addr = inet_addr(ipAddress); // Endereço
             destiny.sin_port = htons(APP_PORT); // Porta de destino
-            sendto(sock, path, ECHOMAX, 0, (struct sockaddr *)&destiny, sizeof(destiny)); // Envio das informações
-            // printf("Pacote enviado\n");
+            sendto(sock, path, strlen(path), 0, (struct sockaddr *)&destiny, sizeof(destiny)); // Envio das informações
         }
-        sleep(interval);
-    } while(stop == 0);
-    // printf("Fechando socket de envio\n");
+
+        // Leitura thread-safe do intervalo
+        pthread_mutex_lock(&input_lock);
+        int current_interval = interval;
+        pthread_mutex_unlock(&input_lock);
+
+        sleep(current_interval);
+    } while (stop == 0);
     close(sock);
 
     return (void *)0; // É necessário cast para evitar warning
 }
 
-// Função que rebe as informações
+// Função que recebe as informações
 void *data_receiver_thread(void *arg) {
     int sock;
-    // Estrutura: familia + endereco IP + porta
     struct sockaddr_in me, from;
-    int adl = sizeof(me);
+    socklen_t adl = sizeof(from); // Corrigido para 'from'
     char linha[ECHOMAX];
 
     // Cria o socket para enviar e receber datagramas parâmetros(familia, tipo, protocolo)
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
         printf("ERRO na Criacao do Socket!\n");
-    else  
+    else
         printf("Servidor esperando mensagens...\n");
 
     // Construcao da estrutura do endereco local, preenchendo a estrutura socket me (familia, IP, porta)
     me.sin_family = AF_INET;
-    me.sin_addr.s_addr=htonl(INADDR_ANY); // Endereco IP local
+    me.sin_addr.s_addr = htonl(INADDR_ANY); // Endereco IP local
     me.sin_port = htons(APP_PORT); // Porta local
 
     // Bind para o endereco local parametros(descritor socket, estrutura do endereco local, comprimento do endereco)
-    if(-1 != bind(sock, (struct sockaddr *)&me, sizeof(me)))
-        do  {
+    if (-1 != bind(sock, (struct sockaddr *)&me, sizeof(me))) {
+        do {
             // Bloqueio por mutex
             pthread_mutex_lock(&input_lock);
-            while (pause_output) { pthread_cond_wait(&input_cond, &input_lock); }
+            while (pause_output) {
+                pthread_cond_wait(&input_cond, &input_lock);
+            }
             pthread_mutex_unlock(&input_lock);
 
-            // Recebe mensagem do endereco remoto - parametros(descritor socket, dados, tamanho dos dados, flag, estrutura do socket remoto, tamanho da estrutura)
-            recvfrom(sock, linha, ECHOMAX, 0, (struct sockaddr *)&from, &adl);
-            printf("%s", linha);
-        }
-        while(stop == 0);
-    else puts("Porta ocupada");
+            // Recebe mensagem do endereco remoto
+            ssize_t recv_len = recvfrom(sock, linha, ECHOMAX - 1, 0, (struct sockaddr *)&from, &adl);
+            if (recv_len > 0) {
+                linha[recv_len] = '\0'; // Garante a terminação nula
+                printf("Recebido: %s\n", linha);
+            }
+        } while (stop == 0);
+    }
+    else {
+        puts("Porta ocupada");
+    }
 
     // Fecha o socket
     close(sock);
     return 0;
-}
-
-//
-void alter_interval(){
-    // Todo: desenvolver para fazer a alteração do intervalo (variável interval)
-    // enviar e receber isso via UDP (está na descrição do trabalho, mas não nos pontos que serão avaliados)
-
-    return (void)0;
 }
 
 // Controle de input do usuário
@@ -200,7 +229,10 @@ void *user_input_thread(void *arg) {
 
     do {
         printf("Aperte enter para usar input\n");
-        fgets(in, sizeof(in), stdin); // Todo: esse input está causando erro, mas o arquivo testebloqueio.c tem algo muito parecido que funciona sem este erro. Corrigir.
+        if (fgets(in, sizeof(in), stdin) == NULL) {
+            perror("fgets");
+            continue;
+        }
 
         pthread_mutex_lock(&input_lock);
         pause_output = 1;  // Pausa o fluxo das outras threads
@@ -208,22 +240,32 @@ void *user_input_thread(void *arg) {
 
         // Bloqueia a execução até o usuário apertar enter
         printf("1: Adicionar IP | 2: Alterar tempo de envio | 0: Finalizar programa \nDigite um número:\n");
-        // input = getchar();
+        input = getchar();
+        while (getchar() != '\n'); // Limpa o buffer
+
         switch (input) {
-        case '1':
-            // Todo: input do usuário e chamar a função add_ip_address
-            break;
-        case '2':
-            // Todo: chamar a função alter_interval
-            break;
-        case '0':
-            stop = 1;
-            break;
-        default:
-            // Todo: aviso de exceção
+            case '1':
+                printf("Digite o IP a ser adicionado: ");
+                if (fgets(in, sizeof(in), stdin) != NULL) {
+                    // Remove o caractere de nova linha
+                    in[strcspn(in, "\n")] = '\0';
+                    add_ip_address(in);
+                    printf("IP %s adicionado.\n", in);
+                }
+                break;
+            case '2':
+                // Chamar a função alter_interval
+                alter_interval();
+                break;
+            case '0':
+                stop = 1;
+                break;
+            default:
+                printf("Opção inválida!\n");
         }
+
         pthread_mutex_lock(&input_lock);
-        pause_output = 0;  // Signal the output thread to resume
+        pause_output = 0;  // Sinaliza para as threads de retomarem
         pthread_cond_signal(&input_cond);
         pthread_mutex_unlock(&input_lock);
     } while (stop == 0);
@@ -231,8 +273,8 @@ void *user_input_thread(void *arg) {
 }
 
 int main() {
-    pthread_t threads[2];
-    int thread_args[2];
+    pthread_t threads[3];
+    int thread_args[3]; 
     int i;
     char local_ip[100];
 
@@ -244,28 +286,41 @@ int main() {
     pthread_mutex_init(&input_lock, NULL);
     pthread_cond_init(&input_cond, NULL);
 
+    // Criação das threads
     i = 0;
     thread_args[i] = i;
-    pthread_create(&threads[i], NULL, data_receiver_thread, (void *)&thread_args[i]);
+    if (pthread_create(&threads[i], NULL, data_receiver_thread, (void *)&thread_args[i]) != 0) {
+        perror("pthread_create data_receiver_thread");
+        exit(EXIT_FAILURE);
+    }
     printf("Thread de recebimento de dados criada.\n");
 
     i = 1;
     thread_args[i] = i;
-    pthread_create(&threads[i], NULL, data_sender_thread, (void *)&thread_args[i]);
+    if (pthread_create(&threads[i], NULL, data_sender_thread, (void *)&thread_args[i]) != 0) {
+        perror("pthread_create data_sender_thread");
+        exit(EXIT_FAILURE);
+    }
     printf("Thread de envio criada.\n");
 
     i = 2;
     thread_args[i] = i;
-    pthread_create(&threads[i], NULL, user_input_thread, (void *)&thread_args[i]);
+    if (pthread_create(&threads[i], NULL, user_input_thread, (void *)&thread_args[i]) != 0) {
+        perror("pthread_create user_input_thread");
+        exit(EXIT_FAILURE);
+    }
     printf("Thread de input criada\n");
 
-    // printf("Juntando 1ª thread\n");
+    // Juntando as threads
     pthread_join(threads[0], NULL);
-    // printf("Juntando 3ª thread\n");
     pthread_join(threads[2], NULL);
-    // printf("Juntando 2ª thread\n");
     pthread_join(threads[1], NULL);
 
     printf("Programa finalizado!\n");
+
+    // Destruir mutex e condição
+    pthread_mutex_destroy(&input_lock);
+    pthread_cond_destroy(&input_cond);
+
     return 0;
 }
